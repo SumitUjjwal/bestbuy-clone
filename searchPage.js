33a526778eee4c8af10542cd7fae4181ce9cd5e3
Mamta305 <mahithakur47363@gmail.com>
2022-12-14 23:52:13 +0530

    an search bar

--- searchPage.js
+++ searchPage.js
@@ -4,7 +4,7 @@ let api="https://6398c0f229930e2bb3c11afd.mockapi.io/mobiles";
 async function fetchBySearch(){
     let req= await fetch(api);
     let res=await req.json();
-    console.log(res);
+    // console.log(res);
 }
 let container=document.getElementById("container");
 function renderedData(data){

ba2d0abfbbb1bb32575fdb25467966f940b3d19f
Bhupender Soni <official.soni532@gmail.com>
2022-12-15 21:00:46 +0530

    render top section cards

--- script/index.js
+++ script/index.js
@@ -1,19 +1,40 @@
-// ! Rendering Top Secion cards
+import data from "../resources/data_files/topSectionObjects.js"
 
 let topSectionCardsMain = document.querySelector('.topSection_cards_innerContainer')
-let cards = []
-let imgVal = 4;
-for (let i = 0; i < 18; i++) {
-    let card = `
+
+topSectionCardsMain.innerHTML = data.map(item => {
+  return  `
     <div class="topSection_card_container">
         <a class="topSection_card_imgBox">
-            <img src="resources/topSectionCards/img_${imgVal+i}.jpg" alt="">
+            <img src="${item.src}" alt="${item.title}">
         </a>
         <a href="" class="topSection_card_textBox">
-             Laptops & Computers
+             ${item.title}
         </a>
     </div>
     `
-    cards.push(card)
-}
-topSectionCardsMain.innerHTML = cards.join("")
+}).join("")
+
+
+
+
+// ! Rendering Top Secion cards
+// let cards = []
+// let cards_title = ['Laptop']
+// let imgVal = 4;
+// for (let i = 0; i < 18; i++) {
+//     let card = `
+//     <div class="topSection_card_container">
+//         <a class="topSection_card_imgBox">
+//             <img src="resources/topSectionCards/img_${imgVal+i}.jpg" alt="">
+//         </a>
+//         <a href="" class="topSection_card_textBox">
+//              Laptops & Computers
+//         </a>
+//     </div>
+//     `
+//     cards.push(card)
+// }
+// topSectionCardsMain.innerHTML = cards.join("")
+
+

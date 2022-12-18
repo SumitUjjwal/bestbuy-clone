1ff722a8dcde7bc42e6b8fb4b3667e8bc3a0b686
SumitUjjwal <sumitonboard@gmail.com>
2022-12-18 15:42:00 +0530

    completed payment page

--- script/paymentPage.js
+++ script/paymentPage.js
@@ -4,27 +4,20 @@ let title = sessionStorage.getItem("title");
 let total_price = sessionStorage.getItem("total_price");
 let tax = sessionStorage.getItem("tax");
 let qty = sessionStorage.getItem("qty");
+let userDetails = JSON.parse(sessionStorage.getItem("user_details"));
 
-let price = (total_price-tax).toFixed(2);
+let price = (total_price - tax).toFixed(2);
 
 let item_details = document.getElementById("item_details");
 let right_item_details = document.getElementById("right_item_details");
 let right_checkout_price = document.getElementById("right_checkout_price");
 let address_subm_btn = document.getElementById("address_subm");
-let form = document.getElementById("add_form");
-let address = document.getElementById("userAddress");
+let user_address = document.getElementById("userAddress");
 
 // setting details on DOM
-item_details.innerHTML = 
-`
-       <img class="product_image"
-              src="${img_src}"
-              alt="">
-       <p class="product_title">${title}</p>
-`;
 
-right_item_details.innerHTML = 
-`
+right_item_details.innerHTML =
+       `
       <img src="${img_src}"
               alt="">
        <p id="title">${title}</p>
@@ -35,8 +28,8 @@ right_item_details.innerHTML =
        </div> 
 `;
 
-right_checkout_price.innerHTML = 
-`
+right_checkout_price.innerHTML =
+       `
       <div class="right_checkout_price_box">
               <p>Item Subtotal</p>
               <p>$${price}</p>
@@ -56,34 +49,13 @@ right_checkout_price.innerHTML =
        </div> 
 `
 
-// adding event listener to address button
-
-address_subm_btn.addEventListener("click", setData);
-
-async function setData(event){
-       event.preventDefault();
-       form.style.display = "none";
-       address.style.display = "block";
-
-       let firstName = document.getElementById("inputFirstName").value;
-       let lastName = document.getElementById("inputLastName").value;
-       let address1 = document.getElementById("inputAddress").value;
-       let address2 = document.getElementById("inputAddress2").value;
-       let city = document.getElementById("inputCity").value;
-       let state = document.getElementById("inputState").value;
-       let zip = document.getElementById("inputZip").value;
-
-       let obj = {
-              firstName,
-              lastName,
-              address1,
-              address2,
-              city,
-              state,
-              zip
-       }
-
-       sessionStorage.setItem("user_details", JSON.stringify(obj));
-
-       
-}
+user_address.innerHTML = 
+`
+       <h4>Billing Details</h4>
+       <p id="userName">${userDetails.firstName} ${userDetails.lastName}</p>
+       <p id="address">${userDetails.address1}, ${userDetails.address2}</p>
+       <p id="city">${userDetails.city}</p>
+       <p id="state">${userDetails.state}</p>
+       <p id="zip">${userDetails.zip}</p>
+       <div><span><a href="#">Edit this address</a></span> | <span><a href="#">Add a new address</a></span></div>
+`

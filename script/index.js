ef3976ea37bbb6f47df045dabc81da4bf1ff14b2
SumitUjjwal <sumitonboard@gmail.com>
2022-12-20 11:41:20 +0530

    index

--- script/index.js
+++ script/index.js
@@ -60,7 +60,7 @@ search.addEventListener('click', () => {
 
     let seachVal = document.querySelector('#inputSearch').value;
     localStorage.setItem('searchVal', seachVal)
-    window.location.href = '/obnoxious-ladybug-6279/html/searchPage.html'
+    window.location.href = 'html/searchPage.html'
 })
 
 
